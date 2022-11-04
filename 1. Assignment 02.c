ec2e8cfaf5bf1e719b78c122cc277e4c66203ea0
buinamkhang582004 <86768423+buinamkhang582004@users.noreply.github.com>
2022-11-04 19:46:52 +0700

    Lecture1-Assignment 02

---
+++ 1. Assignment 02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main(void)
+{
+    printf ("“In memory of Dennis Ritchie,\n");
+    printf ("\t creator of the C programming language \n");
+    printf ("\t and co-creator of the UNIX operating system.”\n ");
+    printf ("Paul and Harvey Deitel\n");
+    getch();
+    return 0;
+}
